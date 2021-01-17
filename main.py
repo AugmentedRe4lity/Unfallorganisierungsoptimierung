c2128eaf651ed9982aabd733ca6d91450ff24a8a
AugmentedRe4lity <adv.prg.aug@gmail.com>
2021-01-17 20:21:49 +0100

    IDK

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pprint import pprint
 from ansi.colour.fg import *
 from ansi.colour import bg
 from random import randint
+import sys
+from os import system
 
 from functions import *
 from variables import log_blend_colours
@@ -9,6 +11,7 @@ from screen import Screen
 from object import Object
 
 if '__main__' == __name__:
+    system('cls')
     width, height = 20, 10
     screen = Screen(width, height)
 
@@ -18,12 +21,12 @@ if '__main__' == __name__:
     draw_weights(screen, weights)
 
     while True:
-        notarzt = Object(randint(0,width-1)*4, randint(0,height-1)*4, 'N', yellow, bg.red, width=width)
+        notarzt = Object(randint(0,width-1)*4, randint(0,height-1)*4, 'N', yellow, bg.white, width=width)
         krankenhaus = Object(randint(0,width-1)*4, randint(0,height-1)*4, '+', red, bg.white, width=width)
-        krankenwagen = Object(randint(0,width-1)*4, randint(0,height-1)*4, 'W', red, bg.yellow, width=width)
+        krankenwagen = Object(randint(0,width-1)*4, randint(0,height-1)*4, 'W', red, bg.white, width=width)
         if notarzt.position != krankenhaus.position and krankenhaus.position != krankenwagen.position:
             break
 
     screen.show_object([notarzt, krankenhaus, krankenwagen])
 
-    screen.display()
+    screen.display(debug=True if len(sys.argv)==2 else False)
--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 from ansi.colour.fg import *
 from ansi.colour import bg
 from os import system
+import platform
 
 class Screen:
     def __init__(self, width, height, foreground=None, background=None):
@@ -13,23 +14,38 @@ class Screen:
     def set(self, x, y, c, background=None, foreground=None):
         background = str(background) if background!=None else str(self.background)
         foreground = str(foreground) if foreground!=None else str(self.foreground)
-        self.screen[y][x] = background+foreground+c+str(self.background)+str(self.foreground)
+        if x>=0 and x<3*self.width+(self.width-3) and y>=0 and y<3*self.height+(self.height-3):
+            self.screen[y][x] = background+foreground+c+str(self.background)+str(self.foreground)
 
     def show_object(self, objects):
-        for object in objects:
-            for x in [-1,0,1]:
-                for y in [-1,0,1]:
-                    if x==0 and y==0:
-                        self.set(object.x, object.y, '\b'+object.label+str(bg.blue)+' ', object.bg, object.fg)
-                    elif x==-1:
-                        self.set(object.x+x, object.y+y, '  ', bg.blue, white)
-                    elif x==1:
-                        self.set(object.x+x, object.y+y, '\b ', bg.blue, white)
-                    else:
-                        self.set(object.x+x, object.y+y, '\b  ', bg.blue, white)
+        # for object in objects:
+        #     for x in [-1,0,1]:
+        #         for y in [-1,0,1]:
+        #             if x==0 and y==0:
+        #                 self.set(object.x, object.y, '\b'+object.label+str(bg.blue)+' ', object.bg, object.fg)
+        #             elif x==-1:
+        #                 self.set(object.x+x, object.y+y, '  ', bg.blue, white)
+        #             elif x==1:
+        #                 self.set(object.x+x, object.y+y, '\b ', bg.blue, white)
+        #             else:
+        #                 self.set(object.x+x, object.y+y, '\b  ', bg.blue, white)
 
-    def display(self):
-        system('clear')
+        for object in objects:
+            self.set(object.x, object.y, object.label, object.bg, object.fg)
+            self.set(object.x-1, object.y-1, '\\')
+            self.set(object.x-1, object.y+1, '/')
+            self.set(object.x+1, object.y+1, '\\')
+            self.set(object.x+1, object.y-1, '/')
+            print(f'{object.label}: {object.x}, {object.y}')
+
+    def display(self, debug = False):
+        os_platform = platform.system()
+        if not debug:
+            if os_platform=='Windows':
+                system('cls')
+            else:
+                system('clear')
+
         print('\n'.join([' '.join([j for j in i]) for i in self.screen]))
 
     def size(self):
